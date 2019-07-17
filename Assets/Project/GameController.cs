41b4bd08016e4604f3b7d514606c97e8e3080dcc
kobernikev@outlook.com <kobernikev@outlook.com>
2019-07-17 22:24:26 +0300

    init commit

---
+++ Assets/Project/GameController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using NDream.AirConsole;
+using Newtonsoft.Json.Linq;
+
+public class GameController : MonoBehaviour
+{
+	public GameObject playerPrefab;
+	
+	public Dictionary<int, PlayerController>players = new Dictionary<int, PlayerController>();
+	private void Awake()
+	{
+		AirConsole.instance.onMessage += OnMessage;
+		AirConsole.instance.onConnect += OnConnect;
+		AirConsole.instance.onReady += OnReady;
+	}
+
+	void OnReady(string code)
+	{
+		List<int> connectedDevices = AirConsole.instance.GetControllerDeviceIds();
+		foreach (int deviceId in connectedDevices)
+		{
+			AddNewPlayer(deviceId);
+		}
+	}
+
+	void OnConnect(int deviceId)
+	{
+		AddNewPlayer(deviceId);	
+	}
+
+	private void AddNewPlayer(int deviceId)
+	{
+		if (players.ContainsKey(deviceId))
+		{
+			return;
+		}
+		
+		GameObject newPlayer = Instantiate(playerPrefab, transform.position, transform.rotation) as GameObject;
+		players.Add(deviceId, newPlayer.GetComponent<PlayerController>());
+	}
+
+	void OnMessage(int from, JToken data)
+	{
+		if (players.ContainsKey(from) && data["action"] != null)
+		{
+			players[from].ButtonInput(data["action"].ToString());
+		}
+	}
+	
+	void OnDestroy () {
+		if (AirConsole.instance != null) {
+			AirConsole.instance.onMessage -= OnMessage;		
+			AirConsole.instance.onReady -= OnReady;		
+			AirConsole.instance.onConnect -= OnConnect;		
+		}
+	}
+}
---
+++ Assets/Project/PlayerController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using NDream.AirConsole;
+using Newtonsoft.Json.Linq;
+
+public class PlayerController : MonoBehaviour
+{
+	public Rigidbody rigidBody;
+	public float movementSpeed = 23f;
+	public bool isMovingForward;
+	public bool isMovingBack;
+	public bool isMovingLeft;
+	public bool isMovingRight;
+	
+
+	private void Start()
+	{
+		rigidBody = GetComponent<Rigidbody>();
+	}
+
+	public void ButtonInput(string input)
+	{
+		switch (input)
+		{
+			case "right":
+				isMovingRight = true;
+				break;
+			case "left":
+				isMovingLeft = true;
+				break;
+			case "forward":
+				isMovingForward = true;
+				break;
+			case "back":
+				isMovingBack = true;
+				break;
+			case "right-up":
+				isMovingRight = false;
+				break;
+			case "left-up":
+				isMovingLeft = false;
+				break;
+			case "forward-up":
+				isMovingForward = false;
+				break;
+			case "back-up":
+				isMovingBack = false;
+				break;
+		}
+	}
+
+	private void MoveLeft()
+	{
+		rigidBody.AddForce(transform.right * -movementSpeed);
+	}
+	private void MoveRight()
+	{
+		rigidBody.AddForce(transform.right * movementSpeed);
+	}
+	private void MoveForward()
+	{
+		rigidBody.AddForce(transform.forward * movementSpeed);
+	}
+	private void MoveBack()
+	{
+		rigidBody.AddForce(-(transform.forward) * movementSpeed);
+	}
+
+	private void FixedUpdate()
+	{
+		if (isMovingLeft && !isMovingRight)
+		{
+			MoveLeft();
+		}
+
+		if (isMovingRight && !isMovingLeft)
+		{
+			MoveRight();
+		}
+
+		if (isMovingForward && !isMovingBack)
+		{
+			MoveForward();
+		}
+
+		if (isMovingBack && !isMovingBack)
+		{
+			MoveBack();
+		}
+	}
+}
